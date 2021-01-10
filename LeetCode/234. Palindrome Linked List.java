170bd9babc920f7f2a3619dad1084237921054cf
Manoj Pathak <manoj.pathak1215@gmail.com>
2021-01-10 18:59:26 +0530

    234. Palindrome Linked List

--- LeetCode/234. Palindrome Linked List.java
+++ LeetCode/234. Palindrome Linked List.java
@@ -1,6 +1,3 @@
-// 234. Palindrome Linked List
-// https://leetcode.com/problems/palindrome-linked-list/
-
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -13,58 +10,24 @@
  */
 class Solution {
   public boolean isPalindrome(ListNode head) {
-    int len = getLinkedListLength(head);
-    if (len == 0) return true;
-    else if (len == 1) return true;
-    int mid = len % 2 == 0 ? (len / 2) : (int)Math.ceil(len / 2);
-    mid = mid - 1;
-    head = reverseHalf(head, mid);
-    mid = mid + 1;
-    ListNode firstPtr = head;
-    ListNode secondPtr = head;
-    while (mid != 0) {
-      mid--;
-      secondPtr = secondPtr.next;
-    }
-    while (firstPtr != null && secondPtr != null && firstPtr.val == secondPtr.val) {
-      System.out.println(firstPtr.val + " - " + secondPtr.val);
-    System.out.println("---------------");
-      firstPtr = firstPtr.next;
-      secondPtr = secondPtr.next;
-    }
-    return firstPtr != null && secondPtr == null;
-  }
-  public ListNode reverseHalf(ListNode head, int mid) {
-    ListNode current = head;
-    while (mid != 0) {
-      mid--;
-      current = current.next;
-    }
-    current.next = reverse(current.next);
-    return head;
-  }
-  public int getLinkedListLength(ListNode head) {
-    int length = 0;
+    Stack<ListNode> stack = new Stack<>();
     ListNode current = head;
     while (current != null) {
-      length++;
+      stack.push(current);
       current = current.next;
     }
-    return length; 
-  }
-  public ListNode reverse(ListNode head) {
-    ListNode next = null;
-    ListNode current = head;
-    ListNode prev = null;
-    while (current != null) {
-      next = current.next;
-      current.next = prev;
-      prev = current;
-      current = next;
+    ListNode reversedList = new ListNode(-1);
+    current = reversedList;
+    while (!stack.isEmpty()) {
+      current.next = new ListNode(stack.pop().val);
+      current = current.next;
     }
-    head = prev;
-    return head;
+    reversedList = reversedList.next;
+    while (head != null) {
+      if (head.val != reversedList.val) return false;
+      head = head.next;
+      reversedList = reversedList.next;
+    }
+    return true;
   }
 }
-
-// TODO: Not able to solve.
