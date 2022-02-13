ddede18c7fad3dbf4e5c8a02875a344368f6b1ae
Manoj Pathak <manoj.pathak1215@gmail.com>
2022-02-13 13:26:21 +0530

    1598. Crawler Log Folder

---
+++ LeetCode/Stack/1598. Crawler Log Folder.java
@@ -0,0 +1,17 @@
+class Solution {
+  public int minOperations(String[] logs) {
+    Stack<String> stack = new Stack<>();
+    for (String log: logs) {
+      switch (log) {
+        case "../":
+          if (!stack.empty()) stack.pop();
+          break;
+        case "./":
+          break;
+        default:
+          stack.push(log);
+      }
+    }
+    return stack.size();
+  }
+}
